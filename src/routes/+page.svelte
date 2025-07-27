71bdad96282c4d740a1ce6bae900530d97f2f368
Nurullah <nurullahan408@gmail.com>
2025-07-27 10:07:51 +0600

    Added a components directory for those that will be used globally

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import Header from './components/Header.svelte';
+
   // Data from server side
   let { data } = $props();
 </script>
-
+<Header />
 <section class="flex flex-col items-center">
   <h1 class="text-3xl font-bold">Pokemon list</h1>
   <ul>
---
+++ src/routes/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+  let routes = [
+    {
+      route: '/pokemon',
+      title: 'Pokemon'
+    }
+  ]
+</script>
+<header>
+  <nav>
+    {#each routes as {route, title} }
+    <a href={route}>{title}</a>
+    {/each}
+  </nav>
+</header>
