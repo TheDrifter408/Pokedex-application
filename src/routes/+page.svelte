72bbe94bc274aa6955be1c8e8df310669e74662b
Nurullah <nurullahan408@gmail.com>
2025-07-28 10:27:48 +0600

    Added some routes and made pokemons searchable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,40 @@
 <script lang="ts">
-  import Header from './components/Header.svelte';
+  import Header from "./components/Header.svelte";
 
-  // Data from server side
-  let { data } = $props();
+  let search = $state('');
+  let pokemon = $state([]);
+
+  const onSearch = async (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      console.log('firing');
+      if (!search.trim()) return;
+
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}/`);
+        const result = await response.json();
+        // Todo Fix the issue search results not turning up in the UI
+        console.log('Results', result);
+        pokemon = result.results;
+      } catch (e) {
+        console.error('Not found');
+      }
+    }
+  }
 </script>
+
 <Header />
 <section class="flex flex-col items-center">
-  <h1 class="text-3xl font-bold">Pokemon list</h1>
-  <ul>
-    {#each data.results as { name, id }}
-      <li>
-        <a href="/pokemon/{id}">{name}</a>
-      </li>
-    {/each}
-  </ul>
+  <h1 class="text-3xl font-bold">Pokedex</h1>
+  <article>
+    <form method="GET">
+      <div class="border rounded-xl w-full">
+        <input name="search" bind:value={search} onkeydown={onSearch} />
+      </div>
+    </form>
+    <div class="grid grid-cols-3">
+      {#each pokemon as { name, id} (id)}
+      <div>{name}</div>
+      {/each}
+    </div>
+  </article>
 </section>
